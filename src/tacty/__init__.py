4edad8e9fc076c6c253b13bbfdf1a9965bf2a5c2
ZyphenVisuals <contact@zyphenvisuals.com>
2026-03-19 08:37:51 +0200

    start migrating to breeze

--- src/tacty/__init__.py
+++ src/tacty/__init__.py
@@ -1,11 +1,12 @@
 import sys
 from importlib.metadata import PackageNotFoundError, version
 
-from PySide6.QtCore import QSettings, Qt
+from PySide6.QtCore import QFile, QIODevice, QSettings, Qt, QTextStream
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QStyleFactory
 
 import tacty.resources.resources_rc  # pyright: ignore[reportUnusedImport] # noqa: F401
+import tacty.resources.themes.breeze_pyside6  # pyright: ignore[reportUnusedImport] # noqa: F401
 
 from .ui.windows import MainWindow
 
@@ -30,9 +31,16 @@ def main() -> None:
     # Load the theme
     app.setStyle(QStyleFactory.create("Fusion"))
     if QSettings().value("lightMode", type=bool):
-        app.styleHints().setColorScheme(Qt.ColorScheme.Light)
+        file = QFile(":/light/stylesheet.qss")
+        _ = file.open(QIODevice.OpenModeFlag.ReadOnly | QIODevice.OpenModeFlag.Text)
+        stream = QTextStream(file)
+        app.setStyleSheet(stream.readAll())
     else:
         app.styleHints().setColorScheme(Qt.ColorScheme.Dark)
+        file = QFile(":/dark/stylesheet.qss")
+        _ = file.open(QIODevice.OpenModeFlag.ReadOnly | QIODevice.OpenModeFlag.Text)
+        stream = QTextStream(file)
+        app.setStyleSheet(stream.readAll())
 
     # Load the main window.
     main_window = MainWindow()
