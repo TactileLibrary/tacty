458464dfdd60521ccf4e02e748226f533efcec63
ZyphenVisuals <rotarujunior@gmail.com>
2026-03-29 01:27:38 +0200

    add reset to default

--- src/tacty/ui/forms/calibration_form.py
+++ src/tacty/ui/forms/calibration_form.py
@@ -23,8 +23,11 @@ class CalibrationForm(QWidget):
 
         # connectors
         _ = self.ui.startFrame.editingFinished.connect(self.updateStartFrame)
+        _ = self.ui.startFrameReset.clicked.connect(self.resetStartFrame)
         _ = self.ui.endFrame.editingFinished.connect(self.updateEndFrame)
+        _ = self.ui.endFrameReset.clicked.connect(self.resetEndFrame)
         _ = self.ui.frameRate.editingFinished.connect(self.updateFrameRate)
+        _ = self.ui.frameRateReset.clicked.connect(self.resetFrameRate)
 
     def updateData(self, data: CalibrationOptions):
         self.data = data
@@ -35,19 +38,40 @@ class CalibrationForm(QWidget):
         self.ui.endFrame.setValue(data.videoTrim.end.value)
         self.ui.frameRate.setValue(data.videoFps.value)
 
-    def updateStartFrame(self):
-        frame = self.ui.startFrame.value()
+    def updateStartFrame(self, frame: int | None = None):
+        if frame is None:
+            frame = self.ui.startFrame.value()
+        else:
+            self.ui.startFrame.setValue(frame)
         self.ui.endFrame.setValue(max(frame, self.ui.endFrame.value()))
         self.data.videoTrim.start.value = frame
         self.dataChanged.emit(self.data)
 
-    def updateEndFrame(self):
-        frame = self.ui.endFrame.value()
+    def resetStartFrame(self):
+        frame = self.data.videoTrim.start.default
+        self.updateStartFrame(frame)
+
+    def updateEndFrame(self, frame: int | None = None):
+        if frame is None:
+            frame = self.ui.endFrame.value()
+        else:
+            self.ui.endFrame.setValue(frame)
         self.ui.startFrame.setValue(min(frame, self.ui.startFrame.value()))
         self.data.videoTrim.end.value = frame
         self.dataChanged.emit(self.data)
 
-    def updateFrameRate(self):
-        fps = self.ui.frameRate.value()
+    def resetEndFrame(self):
+        frame = self.data.videoTrim.end.default
+        self.updateEndFrame(frame)
+
+    def updateFrameRate(self, fps: float | None = None):
+        if fps is None:
+            fps = self.ui.frameRate.value()
+        else:
+            self.ui.frameRate.setValue(fps)
         self.data.videoFps.value = fps
         self.dataChanged.emit(self.data)
+
+    def resetFrameRate(self):
+        fps = self.data.videoFps.default
+        self.updateFrameRate(fps)
--- src/tacty/ui/forms/calibration_ui.py
+++ src/tacty/ui/forms/calibration_ui.py
@@ -16,13 +16,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QDoubleSpinBox, QFormLayout,
-    QLabel, QSizePolicy, QSpinBox, QWidget)
+    QHBoxLayout, QLabel, QPushButton, QSizePolicy,
+    QSpinBox, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(300, 500)
+        Form.resize(528, 500)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -38,43 +39,99 @@ class Ui_Form(object):
         self.startFrameLabel = QLabel(Form)
         self.startFrameLabel.setObjectName(u"startFrameLabel")
 
-        self.formLayout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.startFrameLabel)
+        self.formLayout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.startFrameLabel)
 
         self.endFrameLabel = QLabel(Form)
         self.endFrameLabel.setObjectName(u"endFrameLabel")
 
-        self.formLayout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.endFrameLabel)
+        self.formLayout.setWidget(3, QFormLayout.ItemRole.LabelRole, self.endFrameLabel)
 
+        self.frameRateLabel = QLabel(Form)
+        self.frameRateLabel.setObjectName(u"frameRateLabel")
+
+        self.formLayout.setWidget(4, QFormLayout.ItemRole.LabelRole, self.frameRateLabel)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.startFrame = QSpinBox(Form)
         self.startFrame.setObjectName(u"startFrame")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.startFrame.sizePolicy().hasHeightForWidth())
+        self.startFrame.setSizePolicy(sizePolicy1)
         self.startFrame.setFrame(True)
         self.startFrame.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.PlusMinus)
         self.startFrame.setAccelerated(False)
         self.startFrame.setProperty(u"showGroupSeparator", False)
         self.startFrame.setMaximum(0)
 
-        self.formLayout.setWidget(1, QFormLayout.ItemRole.FieldRole, self.startFrame)
+        self.horizontalLayout.addWidget(self.startFrame)
 
+        self.startFrameReset = QPushButton(Form)
+        self.startFrameReset.setObjectName(u"startFrameReset")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.startFrameReset.sizePolicy().hasHeightForWidth())
+        self.startFrameReset.setSizePolicy(sizePolicy2)
+        self.startFrameReset.setFlat(False)
+
+        self.horizontalLayout.addWidget(self.startFrameReset)
+
+
+        self.formLayout.setLayout(2, QFormLayout.ItemRole.FieldRole, self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.endFrame = QSpinBox(Form)
         self.endFrame.setObjectName(u"endFrame")
+        sizePolicy1.setHeightForWidth(self.endFrame.sizePolicy().hasHeightForWidth())
+        self.endFrame.setSizePolicy(sizePolicy1)
         self.endFrame.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.PlusMinus)
         self.endFrame.setMaximum(0)
 
-        self.formLayout.setWidget(2, QFormLayout.ItemRole.FieldRole, self.endFrame)
+        self.horizontalLayout_2.addWidget(self.endFrame)
 
-        self.frameRateLabel = QLabel(Form)
-        self.frameRateLabel.setObjectName(u"frameRateLabel")
+        self.endFrameReset = QPushButton(Form)
+        self.endFrameReset.setObjectName(u"endFrameReset")
+        sizePolicy2.setHeightForWidth(self.endFrameReset.sizePolicy().hasHeightForWidth())
+        self.endFrameReset.setSizePolicy(sizePolicy2)
+        self.endFrameReset.setFlat(False)
 
-        self.formLayout.setWidget(3, QFormLayout.ItemRole.LabelRole, self.frameRateLabel)
+        self.horizontalLayout_2.addWidget(self.endFrameReset)
 
+
+        self.formLayout.setLayout(3, QFormLayout.ItemRole.FieldRole, self.horizontalLayout_2)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.frameRate = QDoubleSpinBox(Form)
         self.frameRate.setObjectName(u"frameRate")
+        sizePolicy1.setHeightForWidth(self.frameRate.sizePolicy().hasHeightForWidth())
+        self.frameRate.setSizePolicy(sizePolicy1)
         self.frameRate.setMaximum(999.990000000000009)
 
-        self.formLayout.setWidget(3, QFormLayout.ItemRole.FieldRole, self.frameRate)
+        self.horizontalLayout_3.addWidget(self.frameRate)
+
+        self.frameRateReset = QPushButton(Form)
+        self.frameRateReset.setObjectName(u"frameRateReset")
+        sizePolicy2.setHeightForWidth(self.frameRateReset.sizePolicy().hasHeightForWidth())
+        self.frameRateReset.setSizePolicy(sizePolicy2)
+        self.frameRateReset.setFlat(False)
+
+        self.horizontalLayout_3.addWidget(self.frameRateReset)
+
+
+        self.formLayout.setLayout(4, QFormLayout.ItemRole.FieldRole, self.horizontalLayout_3)
 
 
         self.retranslateUi(Form)
+
+        self.startFrameReset.setDefault(False)
+        self.endFrameReset.setDefault(False)
+        self.frameRateReset.setDefault(False)
+
 
         QMetaObject.connectSlotsByName(Form)
     # setupUi
@@ -84,9 +141,12 @@ class Ui_Form(object):
         self.timeControlLabel.setText(QCoreApplication.translate("Form", u"Time control", None))
         self.startFrameLabel.setText(QCoreApplication.translate("Form", u"Start frame", None))
         self.endFrameLabel.setText(QCoreApplication.translate("Form", u"End frame", None))
+        self.frameRateLabel.setText(QCoreApplication.translate("Form", u"Framerate", None))
         self.startFrame.setSpecialValueText("")
         self.startFrame.setSuffix("")
-        self.frameRateLabel.setText(QCoreApplication.translate("Form", u"Framerate", None))
+        self.startFrameReset.setText(QCoreApplication.translate("Form", u"\u21ba", None))
+        self.endFrameReset.setText(QCoreApplication.translate("Form", u"\u21ba", None))
         self.frameRate.setSuffix(QCoreApplication.translate("Form", u" FPS", None))
+        self.frameRateReset.setText(QCoreApplication.translate("Form", u"\u21ba", None))
     # retranslateUi
 
