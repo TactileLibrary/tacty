81101a50baf299aba62f42e8f4376b92289f8f0a
ZyphenVisuals <rotarujunior@gmail.com>
2026-05-01 02:09:44 +0300

    v2 AI model

--- src/tacty/ui/opencv/classifiers/AiClassifier.py
+++ src/tacty/ui/opencv/classifiers/AiClassifier.py
@@ -2,6 +2,7 @@ from typing import cast, override
 
 import cv2
 import numpy as np
+from cv2.ml import LogisticRegression_BATCH
 from cv2.typing import MatLike
 from onnxruntime.capi.onnxruntime_inference_collection import (  # pyright: ignore [reportMissingTypeStubs]
     InferenceSession,
@@ -15,7 +16,7 @@ class AiClassifier(BaseClassifier):
     session: InferenceSession
 
     def __init__(self):
-        model_file = QFile(":/onnx/tacty-ai-classifier-v1-0-circle-1-square.onnx")
+        model_file = QFile(":/onnx/tacty-ai-classifier-v2-32x-0-circle-1-square.onnx")
 
         if not model_file.open(QIODeviceBase.OpenModeFlag.ReadOnly):
             raise FileNotFoundError("Could not find the model file in Qt resources.")
@@ -31,14 +32,18 @@ class AiClassifier(BaseClassifier):
         w: int
         h, w = image.shape[:2]  # pyright: ignore [reportAny]
 
-        if h > 20 or w > 20:
+        if h > 30 or w > 30:
             # scale down the image
-            processed = cv2.resize(image, (20, 20), interpolation=cv2.INTER_AREA)
+            processed = cv2.resize(image, (30, 30), interpolation=cv2.INTER_AREA)
+
+            processed = cv2.copyMakeBorder(
+                processed, 1, 1, 1, 1, borderType=cv2.BORDER_CONSTANT, value=0
+            )
         else:
-            processed = np.zeros((20, 20), dtype=np.uint8)
+            processed = np.zeros((32, 32), dtype=np.uint8)
             # just center the image
-            y_offset = (20 - h) // 2
-            x_offset = (20 - w) // 2
+            y_offset = (32 - h) // 2
+            x_offset = (32 - w) // 2
             processed[y_offset : y_offset + h, x_offset : x_offset + w] = image
 
         normalize = processed.astype(np.float32) / 255.0
@@ -47,12 +52,13 @@ class AiClassifier(BaseClassifier):
 
         outputs = self.session.run(None, {"input": tensor})
 
-        probabilities = cast(np.ndarray, outputs[0])[0]
+        logits = cast(np.ndarray, outputs[0])[0]
+
+        exp_logits = np.exp(logits - np.max(logits))
+        probabilities = exp_logits / exp_logits.sum()
 
         labelIdx = np.argmax(probabilities)
         confidence: float = probabilities[labelIdx]
-
-        print("Confidence", confidence)
 
         if labelIdx == 0:
             label = "Circle"
